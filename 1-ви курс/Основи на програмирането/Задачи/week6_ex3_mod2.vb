879a680c259df82766091c2bcc9c6eaa34e4241b
Atanas Tsvetkov <Atanas_Tsvetkov@epam.com>
2020-11-14 19:15:54 +0200

    week6_ex3_mod2

---
+++ 1-ви курс/Основи на програмирането/Задачи/week6_ex3_mod2.vb
@@ -0,0 +1,23 @@
+Imports System
+Module Module1
+    Sub Main()
+        Dim n, x As Integer
+        Randomize()
+        n = CInt(Math.Ceiling(Rnd() * 19)) + 1
+        Console.WriteLine("----==== G U E S S   T H E   N U M B E R ====----")
+        While x <> n
+            Console.Write("Your chois is between 1 and 20: ")
+            x = CInt(Console.ReadLine())
+            If x = n Then
+                Console.WriteLine("YESSSS!")
+            Else
+                If (x < n) Then
+                    Console.WriteLine("The number is bigger!")
+                Else
+                    Console.WriteLine("The number is smaller!")
+                End If
+            End If
+        End While
+        Console.ReadKey()
+    End Sub
+End Module
